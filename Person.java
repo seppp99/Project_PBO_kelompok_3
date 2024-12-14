941579cce03022126f99af09075eeb0ee7decbf4
seppp99 <149401831+seppp99@users.noreply.github.com>
2024-12-14 14:44:54 +0700

    commit perbaikan

--- Person.java
+++ Person.java
@@ -28,6 +28,5 @@ public abstract class Person {
         this.age = age;
     }
 
-    // Metode abstrak yang harus diimplementasikan oleh kelas turunan
-    public abstract void displayInfo();
+    
 }

b344700306ea79d1979e19aaef109e36a4afe6d7
Abhiramabvr <2338018077@webmail.uad.ac.id>
2024-12-21 14:10:37 +0700

    Add another function + getter setter

--- Praktikum.java
+++ Praktikum.java
@@ -7,5 +7,26 @@ abstract class Praktikum{
         this.slot = slot;
     }
 
+    // Getter dan Setter
+
+    public String getNamaPrak() {
+        return namaPrak;
+    }
+
+    public void setNamaPrak(String namaPrak) {
+        this.namaPrak = namaPrak;
+    }
+
+    public int getSlot() {
+        return slot;
+    }
+
+    public void setSlot(int slot) {
+        this.slot = slot;
+    }
+
+    // Untuk Display
+
     public void display();
+
 }
